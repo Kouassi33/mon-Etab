8beca915870c7a504a94f2f8c2a6818e9a73970b
kouassi <kouassiyves026@gmail.com>
2025-11-14 00:50:03 +0000

    add package

---
+++ src/test/java/com/example/monEtab/MonEtabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.monEtab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonEtabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
